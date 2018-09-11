72f2ff178db6fb5e3bf2314bf1c194cb6b2338e7
Navstar Temp <navtemp@NavstarInc.Net>
2018-09-10 20:15:23 -0400

    updated seed file

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,32 @@ DROP DATABASE IF EXISTS hotel_db;
 CREATE DATABASE hotel_db;
 
 USE hotel_db;
+
+
+-- hjj
+
+DROP DATABASE IF EXISTS hotel_db;
+CREATE DATABASE hotel_db;
+
+USE hotel_db;
+
+CREATE TABLE rooms
+(   id INT NOT NULL auto_increment,
+	available boolean DEFAULT false,
+    createdAt timestamp NOT NULL,
+    updatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    Primary Key (id)
+);
+
+CREATE TABLE tabl
+(
+   id INT NOT NULL auto_increment,
+   nam VARCHAR (30) NOT NULL,
+   phone VARCHAR (12) NOT NULL,
+   email VARCHAR (50) NOT NULL,
+   res_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   num_pary int (10) NOT NULL,
+   available boolean DEFAULT false,
+   Primary Key (id)
+);
+
